68cd2bc53fc767aa040d68fbeef31c62bd1d2e05
ingambe <pierre.tassel@outlook.fr>
2018-07-17 11:23:01 +0200

    resultats

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 ./script_compil.sh
-#./original.sh
-#./deroulage.sh
-#./deroulage_2.sh
-#./deroulage_3.sh
+./original.sh
+./deroulage.sh
+./deroulage_2.sh
+./deroulage_3.sh
 ./ordre.sh
 ./ordre_2.sh
 ./ordre_3.sh
