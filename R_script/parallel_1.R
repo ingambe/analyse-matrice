02ca03f661bd1854c547dd6c2a7827c85f01eddc
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-07-31 14:57:36 +0200

    plot

--- R_script/parallel_1.R
+++ R_script/parallel_1.R
@@ -4,7 +4,7 @@ library(wesanderson)
 setwd("/home/ingambe/Bureau/analyse-matrice/output")
 palette <- wes_palette(n=3,name="Zissou1")
 
-xLabels<-c("Sequential", "Sequential AVX2", "Parallel", "Parallel AVX2", "MKL", "compact", "scatter", "Parallel 2", "Parallel 3")
+xLabels<-c("Sequential", "Seq.AVX2", "Parallel", "Parallel AVX2", "MKL", "compact", "scatter", "Parallel 2", "Parallel 3", "Parallel 4")
 
 seq<-read.table(file = "deroulage2/intel/O3/resultat.txt",sep="\n", header=FALSE)
 seqavx<-read.table(file = "intel/avx/resultat.txt",sep="\n", header=FALSE)
@@ -15,6 +15,7 @@ compact<-read.table(file = "parallel1/affinity/intel/compact.txt",sep="\n", head
 scatter<-read.table(file = "parallel1/affinity/intel/scatter.txt",sep="\n", header=FALSE)
 par2<-read.table(file = "parallel2_avx/intel/O3_vec/resultat.txt",sep="\n", header=FALSE)
 par3<-read.table(file = "parallel3_avx/intel/O3/resultat.txt",sep="\n", header=FALSE)
+par4<-read.table(file = "parallel4_avx/intel/O3/resultat.txt",sep="\n", header=FALSE)
 
 generalDetails<-c(expression(
   italic("Linux kernel = 4.13.0-38"), 
@@ -24,9 +25,9 @@ generalDetails<-c(expression(
 #cicada
 generalHardware<-("Intel Xeon W3520, 2.66GHz GHz, 4 cores, 4 GB RAM")
 
-wvioplot(seq$V1, seqavx$V1,par1$V1,par1avx$V1, mkl$V1, compact$V1, scatter$V1, par2$V1, par3$V1, clip=TRUE, adjust = 1, col=palette[1],names=FALSE)
+wvioplot(seq$V1, seqavx$V1,par1$V1,par1avx$V1, mkl$V1, compact$V1, scatter$V1, par2$V1, par3$V1, par4$V1, clip=TRUE, adjust = 1, col=palette[1],names=FALSE)
 
-axis(1,cex.axis=0.6,at=1:9, labels=xLabels)
+axis(1,cex.axis=0.6,at=1:10, labels=xLabels)
 
 legend("topright", generalDetails, bty = "n", cex=0.8)
 
